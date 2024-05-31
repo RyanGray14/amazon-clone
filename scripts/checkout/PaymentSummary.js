4187115aff03c2a3eeee7384d8fa33ac2cee8b23
ryan <ryangray14@outlook.com>
2024-05-31 22:01:19 +0530

    optimize pay summary

--- scripts/checkout/PaymentSummary.js
+++ scripts/checkout/PaymentSummary.js
@@ -3,14 +3,47 @@ import { updateQuantity, price, shipping } from "../checkout.js";
 
 export function renderPaymentSummary() {
     let quantity = updateQuantity(); 
-    document.querySelector('.payment-summary-quantity').innerHTML = `Items(${quantity})`;
-    document.querySelector('.items-total').innerHTML = 
-    `$${rounding(price)}`;
     const tax = price / 10;
-    document.querySelector('.tax').innerHTML = `$${rounding(tax)}`;
     const added_tax = Number(price) + Number(tax);
-    document.querySelector('.shipping-charges').innerHTML = `$${rounding(shipping)}`;
-    document.querySelector('.with-tax').innerHTML = `$${rounding(added_tax)}`;
     const amount = (quantity)? (added_tax+shipping): 0;
-    document.querySelector('.final-amt').innerHTML = `$${rounding(amount)}`;
+
+    const paymentHtml = 
+    `<div class="payment-summary-title">
+        Order Summary
+    </div>
+
+    <div class="payment-summary-row">
+        <div class = "payment-summary-quantity"> 
+            Items(${quantity}) </div>
+        <div class="payment-summary-money"> 
+            $${rounding(price)} </div>
+    </div>
+
+    <div class="payment-summary-row">
+        <div>Estimated tax (10%):</div>
+        <div class="payment-summary-money"> 
+            $${rounding(tax)} </div>
+    </div>
+
+    <div class="payment-summary-row subtotal-row">
+        <div>Total after tax:</div>
+        <div class="payment-summary-money"> 
+            $${rounding(added_tax)} </div>
+    </div>
+
+    <div class="payment-summary-row">
+        <div>Shipping &amp; handling:</div>
+        <div> $${rounding(shipping)} </div>
+    </div>
+
+    <div class="payment-summary-row total-row">
+        <div>Order total:</div>
+        <div class="payment-summary-money">
+            $${rounding(amount)} </div>
+    </div> 
+
+    <button class="place-order-button button-primary">
+        Place your order </button> `;
+        
+    document.querySelector('.payment-summary').innerHTML = paymentHtml;
 }
