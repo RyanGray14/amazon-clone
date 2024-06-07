0f2026d8e54bd4df03b66bf2b08ab13fbeaae484
ryan <ryangray14@outlook.com>
2024-06-07 11:29:41 +0530

    testing1

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { rounding } from "../scripts/utils.js";
+
+describe('test suite: formatCurrency', () => {
+    it('converts cents to dollars', () => {
+        expect(rounding(2095)).toEqual('20.95');
+    });
+
+    it('works with zero', () => {
+        expect(rounding(0)).toEqual('0.00');
+    });
+
+    it('rounds up to nearest', () => {
+        expect(rounding(2000.5)).toEqual('20.01');
+    });
+}); 
